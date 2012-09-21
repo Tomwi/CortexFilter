a67e817e45b1bdbeed6221f7a1deed7c76554324
Willem Melching <willem.melching@gmail.com>
2012-09-21 16:57:53 +0200

    dma toegevoegd

--- src/i2s.c
+++ src/i2s.c
@@ -1,46 +1,77 @@
 #include "i2s.h"
 
-
-I2S_CFG_Type I2S_TransConf = {
-	I2S_WORDWIDTH_16,
-	I2S_STEREO,
-	I2S_STOP_ENABLE,
-	I2S_RESET_ENABLE,
-	I2S_MASTER_MODE,
-	I2S_MUTE_DISABLE,
-};
-
 I2S_MODEConf_Type I2S_ClkConfig;
 
-/* Inits I2S and stops the transmitter and receiver */
-void initI2S(void){
-	I2S_Init(LPC_I2S);
-	I2S_Stop(LPC_I2S, I2S_TX_MODE);
-	I2S_Stop(LPC_I2S, I2S_RX_MODE);
-	I2S_Start(LPC_I2S);
-}
-
-void initTX(unsigned int freq){
+void initTX(unsigned int freq) {
 	/* Configure pins for I2S Transmitter */
+
+	/* Configure pins for I2S
+	 * 		P0.4 as I2SRX_CLK
+	 * 			- P0.5 as I2SRX_WS
+	 * 			- P0.6 as I2SRX_SDA
+	 * 			- P0.7 as I2STX_CLK
+	 * 			- P0.8 as I2STX_WS
+	 * 			- P0.9 as I2STX_SDA
+	 */
 	PINSEL_CFG_Type PinCfg;
-	PinCfg.Funcnum = 1;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-	PinCfg.Pinnum = 7; // P0.7 as I2STX_CLK
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 8; // P0.8 as I2STX_WS
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 9; // P0.9 as I2STX_SDA
+
+	PinCfg.Portnum = PINSEL_PORT_0;
+	PinCfg.Funcnum = PINSEL_FUNC_1;
+	PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	PinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
+
+	PinCfg.Pinnum = PINSEL_PIN_4; // P0.4 as I2SRX_CLK
 	PINSEL_ConfigPin(&PinCfg);
 
+	PinCfg.Pinnum = PINSEL_PIN_5; // P0.5 as I2SRX_WS
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Pinnum = PINSEL_PIN_6; // P0.6 as I2SRX_SDA
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Pinnum = PINSEL_PIN_7; // P0.7 as I2STX_CLK
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Pinnum = PINSEL_PIN_8; // P0.8 as I2STX_WS
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Pinnum = PINSEL_PIN_9; // P0.9 as I2STX_SDA
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Portnum = PINSEL_PORT_4;
+	PinCfg.Funcnum = PINSEL_FUNC_1;
+	PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	PinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
+	PinCfg.Pinnum = PINSEL_PIN_29;
+	PINSEL_ConfigPin(&PinCfg);
+
+	I2S_CFG_Type I2S_TransConf = { I2S_WORDWIDTH_16, I2S_STEREO,
+			I2S_STOP_ENABLE, I2S_RESET_ENABLE, I2S_MASTER_MODE,
+			I2S_MUTE_DISABLE, };
+
+	/* I2S init */
+	I2S_Init(LPC_I2S);
 	I2S_Config(LPC_I2S, I2S_TX_MODE, &I2S_TransConf);
+	I2S_Stop(LPC_I2S, I2S_TX_MODE);
 	I2S_FreqConfig(LPC_I2S, freq, I2S_TX_MODE);
+
 	I2S_ClkConfig.clksel = I2S_CLKSEL_FRDCLK;
 	I2S_ClkConfig.fpin = I2S_4PIN_DISABLE;
 	I2S_ClkConfig.mcena = I2S_MCLK_ENABLE;
-	I2S_ModeConfig(LPC_I2S,&I2S_ClkConfig,I2S_TX_MODE);
+	I2S_ModeConfig(LPC_I2S, &I2S_ClkConfig, I2S_TX_MODE);
+	//I2S_IRQConfig(LPC_I2S,I2S_TX_MODE,8);
+
+
+
+	I2S_DMAConf_Type I2SDMACfg;
+	I2SDMACfg.DMAIndex = I2S_DMA_1;
+	I2SDMACfg.depth = 4;
+	I2S_DMAConfig(LPC_I2S, &I2SDMACfg, I2S_TX_MODE);
+
+	I2S_Start(LPC_I2S);
+
 }
 
-void TransmitValue(unsigned int val){
+void TransmitValue(unsigned int val) {
 	I2S_Send(LPC_I2S, val);
 }
--- src/main.c
+++ src/main.c
@@ -4,14 +4,45 @@
 #include "lpc17xx_clkpwr.h"
 #include "lpc17xx_systick.h"
 #include "lpc17xx_pinsel.h"
+#include "lpc17xx_gpdma.h"
 
 #include "terminal.h"
 #include "i2s.h"
 
 volatile uint32_t SysTickCount;
+volatile uint32_t miliseconds = 0;
+volatile uint32_t txdata = 0;
+volatile uint32_t txblock[4];
+volatile int ledvalue = 0;
+unsigned long LED_PINS = ((uint32_t) 1 << 22);
+
 
 void SysTick_Handler(void) {
 	SysTickCount++; // increment the SysTick counter
+	miliseconds++;
+
+	if (miliseconds >= 500){
+		miliseconds = 0;
+
+
+		txdata++;
+
+		if (txdata >= 65535){
+			txdata = 0;
+		}
+		txblock[0] = txblock[1] = txblock[2] = txblock[3] = txdata;
+
+		term1PutValue(txdata,TRUE);
+
+		if (ledvalue){
+			GPIO_SetValue(0, LED_PINS);
+			ledvalue = 0;
+		}else{
+			GPIO_ClearValue(0, LED_PINS);
+			ledvalue = 1;
+		}
+
+	}
 }
 
 inline static void delay_ms(uint32_t delayTime) {
@@ -22,33 +53,51 @@ inline static void delay_ms(uint32_t delayTime) {
 		;
 }
 
+void DMA_IRQHandler(void){
+
+	;
+}
+
 int main() {
 
 	SystemInit();
 	SystemCoreClockUpdate();
 
 	SysTick_Config(SystemCoreClock / 1000);
-	unsigned long LED_PINS = ((uint32_t) 1 << 22);
 
 	/* Enable GPIO Clock */
 	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCGPIO, ENABLE);
 	GPIO_SetDir(0, LED_PINS, 1);
 
-	/* Uart Init */
 	uart1Init();
-	initI2S();
 	initTX(44100);
 
-	int val = 0;
-	for(;;val++){
-		delay_ms(500);
-		GPIO_ClearValue(0, LED_PINS);
-		delay_ms(500);
-		GPIO_SetValue(0, LED_PINS);
-		TransmitValue((val)|(val<<16));
 
-		term1PutValue(val,FALSE);
-		term1PutValue(val<<16,TRUE);
+
+
+	GPDMA_Init();
+
+
+
+	GPDMA_Channel_CFG_Type DMACfg;
+	DMACfg.ChannelNum = 0;
+	DMACfg.TransferSize = 4;
+	DMACfg.TransferWidth = GPDMA_WIDTH_WORD;
+	DMACfg.SrcMemAddr = txblock;
+	DMACfg.TransferType = GPDMA_TRANSFERTYPE_M2P;
+	DMACfg.SrcConn = GPDMA_CONN_I2S_Channel_0;
+	DMACfg.DMALLI = 0;
+
+
+	//GPDMA_Setup(&DMACfg);
+	//GPDMA_ChannelCmd(0,ENABLE);
+
+
+
+	while(1){
+		//TransmitValue((txdata)|(txdata<<16));
+		;
+
 
 	}
 
