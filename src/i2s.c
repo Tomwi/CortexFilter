59118f99d29aa9dfca0cfd2945be3665729f086b
Willem Melching <willem.melching@gmail.com>
2012-10-06 23:05:56 +0200

    Revert "Code nu nog meer gelijk"
    This reverts commit 12ed2a1133bde6a33867d41c76e9d35fd0ccbee7.

--- src/i2s.c
+++ src/i2s.c
@@ -1,12 +1,19 @@
 #include "i2s.h"
 
-void initTX(unsigned int freq, uint32_t txblock, volatile uint32_t * TC, volatile uint32_t * Err) {
+I2S_MODEConf_Type I2S_ClkConfig;
 
+void initTX(unsigned int freq) {
+	/* Configure pins for I2S Transmitter */
+
+	/* Configure pins for I2S
+	 * 		P0.4 as I2SRX_CLK
+	 * 			- P0.5 as I2SRX_WS
+	 * 			- P0.6 as I2SRX_SDA
+	 * 			- P0.7 as I2STX_CLK
+	 * 			- P0.8 as I2STX_WS
+	 * 			- P0.9 as I2STX_SDA
+	 */
 	PINSEL_CFG_Type PinCfg;
-	I2S_CFG_Type I2S_ConfigStruct;
-	I2S_DMAConf_Type I2SDMACfg;
-	GPDMA_Channel_CFG_Type GPDMACfg;
-	I2S_MODEConf_Type I2S_ClkConfig;
 
 	PinCfg.Portnum = PINSEL_PORT_0;
 	PinCfg.Funcnum = PINSEL_FUNC_1;
@@ -32,74 +39,35 @@ void initTX(unsigned int freq, uint32_t txblock, volatile uint32_t * TC, volatil
 	PINSEL_ConfigPin(&PinCfg);
 
 	PinCfg.Portnum = PINSEL_PORT_4;
+	PinCfg.Funcnum = PINSEL_FUNC_1;
+	PinCfg.OpenDrain = PINSEL_PINMODE_NORMAL;
+	PinCfg.Pinmode = PINSEL_PINMODE_TRISTATE;
 	PinCfg.Pinnum = PINSEL_PIN_29;
 	PINSEL_ConfigPin(&PinCfg);
 
+	I2S_CFG_Type I2S_TransConf = { I2S_WORDWIDTH_16, I2S_STEREO,
+			I2S_STOP_ENABLE, I2S_RESET_ENABLE, I2S_MASTER_MODE,
+			I2S_MUTE_DISABLE, };
+
 	/* I2S init */
 	I2S_Init(LPC_I2S);
-
-	I2S_ConfigStruct.wordwidth = I2S_WORDWIDTH_16;
-	I2S_ConfigStruct.mono = I2S_STEREO;
-	I2S_ConfigStruct.stop = I2S_STOP_ENABLE;
-	I2S_ConfigStruct.reset = I2S_RESET_ENABLE;
-	I2S_ConfigStruct.ws_sel = I2S_MASTER_MODE;
-	I2S_ConfigStruct.mute = I2S_MUTE_DISABLE;
-	I2S_Config(LPC_I2S, I2S_TX_MODE, &I2S_ConfigStruct);
+	I2S_Config(LPC_I2S, I2S_TX_MODE, &I2S_TransConf);
+	I2S_Stop(LPC_I2S, I2S_TX_MODE);
+	I2S_FreqConfig(LPC_I2S, freq, I2S_TX_MODE);
 
 	I2S_ClkConfig.clksel = I2S_CLKSEL_FRDCLK;
 	I2S_ClkConfig.fpin = I2S_4PIN_DISABLE;
 	I2S_ClkConfig.mcena = I2S_MCLK_ENABLE;
 	I2S_ModeConfig(LPC_I2S, &I2S_ClkConfig, I2S_TX_MODE);
+	//I2S_IRQConfig(LPC_I2S,I2S_TX_MODE,8);
 
-	I2S_FreqConfig(LPC_I2S, freq, I2S_TX_MODE);
-
-	/* Initialize GPDMA controller */
-	GPDMA_Init();
-	LPC_GPDMA->DMACConfig = 0x01;
-
-	/* Setting GPDMA interrupt */
-	// Disable interrupt for DMA
-	NVIC_DisableIRQ(DMA_IRQn);
-	/* preemption = 1, sub-priority = 1 */
-	NVIC_SetPriority(DMA_IRQn, ((0x01 << 3) | 0x01));
-
-	/* Setup GPDMA channel --------------------------------*/
-	/* channel 0 */
-	GPDMACfg.ChannelNum = 0;
-	/* Source memory */
-	GPDMACfg.SrcMemAddr = txblock;
-	/* Destination memory */
-	GPDMACfg.DstMemAddr = 0;
-	/* Transfer size */
-	GPDMACfg.TransferSize = 4;
-	/* Transfer width */
-	GPDMACfg.TransferWidth = 0;
-	/* Transfer type */
-	GPDMACfg.TransferType = GPDMA_TRANSFERTYPE_M2P;
-	/* Source connection - unused */
-	GPDMACfg.SrcConn = 0;
-	/* Destination connection - I2S */
-	GPDMACfg.DstConn = GPDMA_CONN_I2S_Channel_0;
-	/* Linker List Item - unused */
-	GPDMACfg.DMALLI = 0;
-	/* Setup channel with given parameter */
-	GPDMA_Setup(&GPDMACfg);
-
-	/* Reset terminal counter */
-	*TC = 0;
-	/* Reset Error counter */
-	*Err = 0;
-
-	/* Enable GPDMA channel 0 */
-	GPDMA_ChannelCmd(0, ENABLE);
-	/* Enable GPDMA interrupt */
-	NVIC_EnableIRQ(DMA_IRQn);
-
+	I2S_DMAConf_Type I2SDMACfg;
 	I2SDMACfg.DMAIndex = I2S_DMA_1;
 	I2SDMACfg.depth = 4;
 	I2S_DMAConfig(LPC_I2S, &I2SDMACfg, I2S_TX_MODE);
 
 	I2S_Start(LPC_I2S);
+
 	I2S_DMACmd(LPC_I2S, I2S_DMA_1, I2S_TX_MODE, ENABLE);
 
 }
