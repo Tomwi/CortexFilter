729cb6700ff34d8423906b0eed9cd14d2d98f63a
Willem Melching <willem.melching@gmail.com>
2012-11-13 14:56:46 +0100

    Stereo -> Mono

--- src/main.c
+++ src/main.c
@@ -24,8 +24,7 @@
 #define ORIGINAL 50
 
 // array to hold input samples
-int16_t insampL[BUFFER_LEN];
-int16_t insampR[BUFFER_LEN];
+int16_t insamp[BUFFER_LEN];
 int16_t coeffs[FILTER_LEN] = { 11, 10, 8, 6, 4, 1, -2, -6, -12, -19, -27, -36,
 		-46, -57, -68, -80, -92, -102, -112, -119, -124, -125, -121, -113, -99,
 		-79, -52, -17, 24, 73, 130, 195, 267, 344, 428, 516, 608, 701, 796, 890,
@@ -38,29 +37,26 @@ int16_t coeffs[FILTER_LEN] = { 11, 10, 8, 6, 4, 1, -2, -6, -12, -19, -27, -36,
 volatile uint32_t SysTickCount;
 volatile uint32_t miliseconds = 0;
 
-volatile uint32_t txBlock1[TRANSFER_SIZE];
-volatile uint32_t rxBlock1[TRANSFER_SIZE];
-volatile uint32_t txBlock2[TRANSFER_SIZE];
-volatile uint32_t rxBlock2[TRANSFER_SIZE];
+volatile uint32_t buffer1[TRANSFER_SIZE];
+volatile uint32_t buffer2[TRANSFER_SIZE];
+volatile uint32_t buffer3[TRANSFER_SIZE];
 
-volatile uint8_t currentBuffer = 1;
 volatile uint8_t needsProcessing = 0;
 
-volatile uint8_t txReady, rxReady;
-
-volatile uint32_t *txActive, *rxActive;
+volatile uint8_t txReady = 0, rxReady = 0;
+volatile uint32_t *txActive, *rxActive, *processActive;
 
 volatile int ledvalue = 0;
 
 void DMA_IRQHandler(void);
 
 // FIR init
-void firFixedLInit(void) {
-	memset(insampL, 0, sizeof(insampL));
+void firFixedInit(void) {
+	memset(insamp, 0, sizeof(insamp));
 }
 
 // the FIR filter function
-void firFixedL(int16_t *coeffs, int16_t *input, int16_t *output, int length,
+void firFixed(int16_t *coeffs, int16_t *input, int16_t *output, int length,
 		int filterLength) {
 	int32_t acc; // accumulator for MACs
 	int16_t *coeffp; // pointer to coefficients
@@ -69,13 +65,13 @@ void firFixedL(int16_t *coeffs, int16_t *input, int16_t *output, int length,
 	int k;
 
 	// put the new samples at the high end of the buffer
-	memcpy(&insampL[filterLength - 1], input, length * sizeof(int16_t));
+	memcpy(&insamp[filterLength - 1], input, length * sizeof(int16_t));
 
 	// apply the filter to each input sample
 	for (n = 0; n < length; n++) {
 		// calculate output n
 		coeffp = coeffs;
-		inputp = &insampL[filterLength - 1 + n];
+		inputp = &insamp[filterLength - 1 + n];
 		// load rounding constant
 		acc = 1 << 14;
 		// perform the multiply-accumulate
@@ -94,65 +90,7 @@ void firFixedL(int16_t *coeffs, int16_t *input, int16_t *output, int length,
 	}
 
 	// shift input samples back in time for next time
-	memmove(&insampL[0], &insampL[length],
-			(filterLength - 1) * sizeof(int16_t));
-}
-
-// FIR init
-void firFixedRInit(void) {
-	memset(insampR, 0, sizeof(insampR));
-}
-
-// the FIR filter function
-void firFixedR(int16_t *coeffs, int16_t *input, int16_t *output, int length,
-		int filterLength) {
-	int32_t acc; // accumulator for MACs
-	int16_t *coeffp; // pointer to coefficients
-	int16_t *inputp; // pointer to input samples
-	int n;
-	int k;
-
-	// put the new samples at the high end of the buffer
-	memcpy(&insampR[filterLength - 1], input, length * sizeof(int16_t));
-
-	// apply the filter to each input sample
-	for (n = 0; n < length; n++) {
-		// calculate output n
-		coeffp = coeffs;
-		inputp = &insampR[filterLength - 1 + n];
-		// load rounding constant
-		acc = 1 << 14;
-		// perform the multiply-accumulate
-		for (k = 0; k < filterLength; k++) {
-			acc += (int32_t)(*coeffp++) * (int32_t)(*inputp--);
-		}
-		// saturate the result
-		if (acc > 0x3fffffff) {
-			acc = 0x3fffffff;
-		} else if (acc < -0x40000000) {
-			acc = -0x40000000;
-		}
-		// convert from Q30 to Q15
-		output[n] = (output[n] * ORIGINAL) / 100
-				+ ((int16_t)(acc >> 15) * FILTERED) / 100;
-	}
-
-	// shift input samples back in time for next time
-	memmove(&insampR[0], &insampR[length],
-			(filterLength - 1) * sizeof(int16_t));
-}
-
-void SysTick_Handler(void) {
-	SysTickCount++; // increment the SysTick counter
-	miliseconds++;
-}
-
-inline static void delay_ms(uint32_t delayTime) {
-	uint32_t currentSysTickCount;
-
-	currentSysTickCount = SysTickCount;
-	while ((SysTickCount - currentSysTickCount) < delayTime)
-		;
+	memmove(&insamp[0], &insamp[length], (filterLength - 1) * sizeof(int16_t));
 }
 
 void DMA_IRQHandler(void) {
@@ -206,15 +144,10 @@ void DMA_IRQHandler(void) {
 
 }
 
-void deInterleave(void* in, void* out, int len);
-
 int main() {
-
 	SystemInit();
 	SystemCoreClockUpdate();
-	SysTick_Config(SystemCoreClock / 1000);
 
-	/* Enable GPIO Clock */
 	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCGPIO, ENABLE);
 	CLKPWR_ConfigPPWR(CLKPWR_PCONP_PCI2S, ENABLE);
 
@@ -223,13 +156,12 @@ int main() {
 
 	uart1Init();
 
-	firFixedLInit();
-	firFixedRInit();
+	firFixedInit();
 
-	rxReady = 0;
-	rxReady = 0;
-	txActive = txBlock1;
-	rxActive = rxBlock1;
+	processActive = buffer3;
+	rxActive = buffer1;
+	txActive = buffer2;
+
 	initTX(44100, (uint32_t) txActive, (uint32_t) rxActive);
 
 	term1PutText("Booted\n\r");
@@ -241,34 +173,28 @@ int main() {
 		if (needsProcessing) {
 			GPIO_SetValue(0, (1 << 22));
 
-			if (currentBuffer == 1) {
-				currentBuffer = 2;
-
-				txActive = txBlock2;
-				rxActive = rxBlock2;
-			} else {
-				currentBuffer = 1;
-
-				txActive = txBlock1;
-				rxActive = rxBlock1;
-			}
-
 			uint32_t i;
 
-			int16_t left[TRANSFER_SIZE];
-			int16_t right[TRANSFER_SIZE];
+			int16_t samples[TRANSFER_SIZE];
+
 
 			for (i = 0; i < TRANSFER_SIZE; i++) {
-				left[i] = rxActive[i] & (0xffff);
-				right[i] = ((int) rxActive[i]) >> 16;
+				samples[i] = (processActive[i] >> 16);
+
 			}
 
-			firFixedL(coeffs, left, left, TRANSFER_SIZE, FILTER_LEN);
-			firFixedR(coeffs, right, right, TRANSFER_SIZE, FILTER_LEN);
+			firFixed(coeffs, samples, samples, TRANSFER_SIZE, FILTER_LEN);
+
 
 			for (i = 0; i < TRANSFER_SIZE; i++) {
-				txActive[i] = ((right[i] << 16) | (left[i] & 0xffff));
+				processActive[i] = samples[i];
 			}
+
+			//rotate buffers
+			volatile uint32_t *tmp = processActive;
+			processActive = rxActive;
+			rxActive = txActive;
+			txActive = tmp;
 
 			needsProcessing = 0;
 			GPIO_ClearValue(0, (1 << 22));
